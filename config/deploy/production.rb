6ee93727c492e239180cff37fb4af773bac83538
Kamil Sluszniak <kamil.sluszniak@gmail.com>
2020-10-31 19:05:26 +0100

    Change production branch to master

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,4 +9,4 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 server '18.224.94.154', user: 'ubuntu', roles: %w[web app db]
-set :branch, 'triggers_check_runner'
+set :branch, 'master'
