aa89a94cb3578627abdefd182768b36802add05b
Kamil Sluszniak <kamil.sluszniak@gmail.com>
2021-02-12 17:15:41 +0100

    Add parsing of string measurements

--- app/services/measurements/writer.rb
+++ app/services/measurements/writer.rb
@@ -2,6 +2,8 @@
 
 module Measurements
   class Writer < Measurements::MeasurementsBase
+    FLOAT_PATTERN = /\d+\.\d+/.freeze
+
     def call(measurements_array)
       data_point = map_metrics_array_to_data_point(measurements_array)
       write_api.write(data: data_point)
@@ -13,9 +15,20 @@ module Measurements
       point = InfluxDB2::Point.new(name: @device_name)
 
       arr.each do |metric|
-        point.add_field(*metric.first)
+        parsed = metric.transform_values { |val| detect_type_and_convert(val) }
+        point.add_field(*parsed.first)
       end
       point
+    end
+
+    def detect_type_and_convert(value)
+      return value unless value.is_a? String
+
+      if value.match(FLOAT_PATTERN)
+        value.to_f
+      else
+        value.to_i
+      end
     end
   end
 end
